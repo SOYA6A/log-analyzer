7abf43cd2561fbc0c6ce7120c9d208434c7bef32
Benlahcen <benlahcen.assia@gmail.com>
2025-12-12 23:56:36 +0100

    fonction annlyse

--- log-analyzer.py
+++ log-analyzer.py
@@ -1,3 +1,9 @@
 import re
 
 def analyser_logs(fichier_log):
+    """
+    Fonction qui analyse un fichier de logs et extrait les IPs.
+    
+    Arguments:
+        fichier_log -- Le chemin du fichier de logs à analyser
+    """
