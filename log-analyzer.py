155ee5e9f3eaefbe6edd130358f8c0dcf054d267
Benlahcen <benlahcen.assia@gmail.com>
2025-12-13 22:20:37 +0100

    coms

--- log-analyzer.py
+++ log-analyzer.py
@@ -1,3 +1,5 @@
+# Importer la bibliothèque 're' pour utiliser les expressions régulières
+# (ça sert à chercher des patterns dans du texte, comme trouver une IP)
 import re
 
 def analyser_logs(fichier_log):
@@ -7,3 +9,4 @@ def analyser_logs(fichier_log):
     Arguments:
         fichier_log -- Le chemin du fichier de logs à analyser
     """
+    compteur_ips = {}
